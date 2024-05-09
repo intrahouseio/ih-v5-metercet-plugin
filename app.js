0a5942de9e035c4ffaf50933ab521e5eb2a706ad
nimagrolis <486016@gmail.com>
2024-05-10 01:01:36 +0300

    v5.0.7

--- app.js
+++ app.js
@@ -3,31 +3,24 @@
  *
  *   Основной модуль плагина
  *   - запрашивает и формирует каналы
- *   - запускает агента для опроса счетчиков
+ *   - инициализирует объект meters для работы с массивом счетчиков
+ *   - запускает агента для опроса
  *   - слушает событие изменения каналов. Это могут быть:
  *        - добавление-удаление узлов (счетчиков)
  *        - изменение парметров каналов (r:1/0, polltimefctr: - изменение интервала опроса)
  *   - по событию изменения
  *       - запрашивает и формирует каналы заново
- *
  */
 const util = require('util');
 
 const Agent = require('./lib/agent');
 const meters = require('./lib/meters');
-// const meterlistformer = require('./lib/meterlistformer');
 
 module.exports = async function(plugin) {
   let agent;
   try {
     meters.init(plugin);
     await getAndCreateMeterlist();
-  } catch (err) {
-    plugin.log('Для работы плагина требуется версия системы не ниже 5.17.25');
-    plugin.exit(17);
-  }
-
-  try {
     agent = new Agent(plugin, plugin.params);
     agent.run();
 
@@ -39,7 +32,6 @@ module.exports = async function(plugin) {
     plugin.log('ERROR: ' + util.inspect(err));
     plugin.exit(2);
   }
-
 
   async function getAndCreateMeterlist() {
     const devhard = await plugin.devhard.get();
--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 /**
  * index.js
- *   Точка входа плагина  
+ *   Точка входа плагина
  */
 
-const util = require("util");
+const util = require('util');
 
-const app = require("./app");
-
+const app = require('./app');
 
 (async () => {
- 
   let plugin;
   try {
     const opt = getOptFromArgs();
     const pluginapi = opt && opt.pluginapi ? opt.pluginapi : 'ih-plugin-api';
-    plugin = require(pluginapi+'/index.js')();
-    
-    plugin.log("Plugin has started.", 1);
-    plugin.params = await plugin.params.get();
-    plugin.log('Received params...');
-    plugin.log("Params:" + util.inspect(plugin.params), 1);
-    await app(plugin);
+    plugin = require(pluginapi + '/index.js')();
+    plugin.log('Plugin has started.', 1);
+    if (!opt.version) {
+      plugin.exit(17, 'Для работы плагина требуется версия системы не ниже 5.17.25!');
+    } else {
+      plugin.params = await plugin.params.get();
+      plugin.log('Received params...');
+      plugin.log('Params:' + util.inspect(plugin.params), 1);
+      await app(plugin);
+    }
   } catch (err) {
-    plugin.exit(8, `Error! Message: ${util.inspect(err)}`);
+    plugin.exit(8, 'ERROR: ' + util.inspect(err));
   }
 })();
-
-
 
 function getOptFromArgs() {
   let opt;
